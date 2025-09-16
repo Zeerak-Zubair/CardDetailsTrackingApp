5dd89733d9a288e50a61305b86f4c1760f2f963e
Zeerak Zubair <zeerak.zubair@stablelogic.com>
2025-09-16 12:47:52 +0500

    Project initialization

---
+++ src/main/kotlin/com/example/cardetailstrackingapp/CarDetailsTrackingAppApplication.kt
@@ -0,0 +1,11 @@
+package com.example.cardetailstrackingapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CarDetailsTrackingAppApplication
+
+fun main(args: Array<String>) {
+    runApplication<CarDetailsTrackingAppApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/cardetailstrackingapp/CarDetailsTrackingAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.cardetailstrackingapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CarDetailsTrackingAppApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
